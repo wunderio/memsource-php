0eb5751774a90d4e51aefa74822611488a6326b4
Jani Palsamäki <jani.palsamaki@gmail.com>
2016-07-21 14:08:05 +0300

    Added some unit tests.

--- tests/src/Memsource/Tests/MemsourceTest.php
+++ tests/src/Memsource/Tests/MemsourceTest.php
@@ -3,13 +3,18 @@
 namespace Memsource\Tests;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Promise\Promise;
+use GuzzleHttp\Promise\PromiseInterface;
 use Memsource\Memsource;
+use Memsource\Model\Parameters;
 use Prophecy\Argument;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class MemsourceTest extends MemsourceTestCase {
+
+  const PATH = 'http://www.example.com/path';
 
   /** @var Client */
   private $client;
@@ -22,6 +27,7 @@ class MemsourceTest extends MemsourceTestCase {
 
     $this->client = $this->prophesize(Client::class);
     $this->client->post(Argument::any(), Argument::any())->willReturn($this->response);
+    $this->client->postAsync(Argument::any(), Argument::any())->willReturn(new Promise());
 
     $this->memsource = new Memsource(self::MEMSOURCE_TEST_BASE_URL, $this->client->reveal());
   }
@@ -34,5 +40,23 @@ class MemsourceTest extends MemsourceTestCase {
 
     $this->assertInstanceOf(JsonResponse::class, $response);
     $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+  }
+
+  /**
+   * @test
+   */
+  public function postShouldReturnJsonResponse() {
+    $response = $this->memsource->post(self::PATH, new Parameters());
+
+    $this->assertInstanceOf(JsonResponse::class, $response);
+  }
+
+  /**
+   * @test
+   */
+  public function postAsyncShouldReturnPromise() {
+    $response = $this->memsource->postAsync(self::PATH, new Parameters());
+
+    $this->assertInstanceOf(PromiseInterface::class, $response);
   }
 }
