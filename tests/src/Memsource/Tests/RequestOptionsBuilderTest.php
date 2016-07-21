f46d3a25d7bea22ebd9a9ece14de18a97b213cff
Jani Palsamäki <jani.palsamaki@gmail.com>
2016-07-21 14:38:05 +0300

    Added some unit tests for RequestOptionsBuilder.

---
+++ tests/src/Memsource/Tests/RequestOptionsBuilderTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Memsource\Tests;
+
+use GuzzleHttp\RequestOptions;
+use Memsource\Model\Parameters;
+use Memsource\RequestOptionsBuilder;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\File\File;
+
+class RequestOptionsBuilderTest extends TestCase {
+
+  /** @var File */
+  private $file;
+
+  /** @var Parameters */
+  private $parameters;
+
+  /** @var RequestOptionsBuilder */
+  private $requestOptionsBuilder;
+
+  public function setUp() {
+    $this->file = new File(__DIR__ . '/test-file.txt');
+    $this->parameters = new Parameters();
+    $this->requestOptionsBuilder = new RequestOptionsBuilder();
+  }
+
+  /**
+   * @test
+   */
+  public function buildPostOptionsShouldUseFormParamsOptionWhenFileIsNotGiven() {
+    $options = $this->requestOptionsBuilder->buildPostOptions($this->parameters);
+
+    $this->assertArrayHasKey(RequestOptions::FORM_PARAMS, $options);
+  }
+
+  /**
+   * @test
+   */
+  public function buildPostOptionsShouldUseMultipartOptionWhenFileIsGiven() {
+    $options = $this->requestOptionsBuilder->buildPostOptions($this->parameters, $this->file);
+
+    $this->assertArrayHasKey(RequestOptions::MULTIPART, $options);
+  }
+}
